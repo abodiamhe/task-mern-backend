032b6516781c4b452d65bebe31c343746fca5e95
unknown <akhigbesadiq@gmail.com>
2025-08-19 23:55:52 +0100

    Cors added

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
-const express = require('express')
-const dotenv = require('dotenv').config()
-const connectDB = require('./connect/database')
+const express = require('express');
+const dotenv = require('dotenv').config();
+const connectDB = require('./connect/database');
+const Cors = require('cors');
 
-const { errorHandler } = require('./middleware/errorMiddleware')
+const { errorHandler } = require('./middleware/errorMiddleware');
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 //Connecting to database when our app start
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
 //middleware to parse incoming json body
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(Cors());
 
 //
-app.use('/api/tasks', require('./routes/taskRoutes'))
+app.use('/api/tasks', require('./routes/taskRoutes'));
 
-app.use('/api/users', require('./routes/userRoutes'))
+app.use('/api/users', require('./routes/userRoutes'));
 
 //Error middleware handler
-app.use(errorHandler)
+app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server listening on port ${port}.`))
+app.listen(port, () => console.log(`Server listening on port ${port}.`));
